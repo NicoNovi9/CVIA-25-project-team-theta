e84990aadef00ee9b532b107825be4e31a7f80d3
NicoNovi9 <nicolanoventa9@gmail.com>
2025-11-17 16:18:17 +0100

    Update .gitignore to exclude error and output files; enhance meluxina.sh for training script execution; improve train.py with dataset preparation and detailed training logs.

--- scripts/meluxina.sh
+++ scripts/meluxina.sh
@@ -1,11 +1,15 @@
-#!/bin/bash 
-#SBATCH --job-name=monitoring_stack #SBATCH --nodes=1 
+#!/bin/bash -l
+#SBATCH --job-name=train 
+#SBATCH --nodes=1 
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=2 
-#SBATCH --mem=8G 
+#SBATCH --partition=gpu 
 #SBATCH --time=02:00:00 
 #SBATCH --qos=default 
-#SBATCH --partition=cpu 
 #SBATCH --account=p200981 
-#SBATCH --output=output/logs/monitoring_stack.out 
-#SBATCH --error=output/logs/monitoring_stack.err
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+
+echo "Starting training script..."
+# python src/test_pytorch_version.py
+python -u src/train.py
--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as T
+from torch.utils.data import Subset
+
 
 from simple_model import SimpleDetector
 from spark_detection_dataset import SparkDetectionDataset
@@ -9,6 +11,8 @@ from spark_detection_dataset import SparkDetectionDataset
 
 if __name__ == "__main__":
     DATA_ROOT = "/project/scratch/p200981/spark2024"
+
+    print("Preparing dataset...", flush=True)
 
     transform = T.Compose([
         T.Resize((256, 256)),
@@ -22,7 +26,10 @@ if __name__ == "__main__":
         transform=transform
     )
 
+    train_dataset = Subset(train_dataset, range(500))
     loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0)
+
+    print("Initializing model...")
 
     model = SimpleDetector(num_classes=10)
     model = model.to("cuda" if torch.cuda.is_available() else "cpu")
@@ -35,7 +42,16 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
 
+    print("Starting training...")
+
     for epoch in range(50):
+        print(f"Starting epoch {epoch+1:03d}")
+
+        total_loss = 0.0
+        total_cls_loss = 0.0
+        total_bbox_loss = 0.0
+        num_batches = 0
+
         for imgs, bboxes, labels in loader:
 
             imgs = imgs.to(device)
@@ -46,12 +62,23 @@ if __name__ == "__main__":
 
             loss_cls = ce_loss(logits, labels)
             loss_bbox = bbox_loss_fn(pred_bbox, bboxes)
-
             loss = loss_cls + loss_bbox
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            print(f"Loss: {loss.item():.4f} | cls: {loss_cls.item():.4f} | bbox: {loss_bbox.item():.4f}")
-        print(f"Epoch {epoch} done.")
+            total_loss += loss.item()
+            total_cls_loss += loss_cls.item()
+            total_bbox_loss += loss_bbox.item()
+            num_batches += 1
+
+        avg_loss = total_loss / num_batches
+        avg_cls_loss = total_cls_loss / num_batches
+        avg_bbox_loss = total_bbox_loss / num_batches
+
+        print(f"Epoch {epoch+1:03d} | "
+            f"Loss: {avg_loss:.4f} | "
+            f"Cls: {avg_cls_loss:.4f} | "
+            f"BBox: {avg_bbox_loss:.4f}")
+
